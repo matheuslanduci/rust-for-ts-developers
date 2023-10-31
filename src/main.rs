ec9555d8c37384d76de5feea97f935d8d5e2df1a
matheuslanduci <matheuslanduci@gmail.com>
2023-10-31 18:38:37 -0300

    feat: borrow checker

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ fn add_one(item: &mut Item) {
     item.count += 1;
 }
 
+fn print_all(items: &Vec<Item>) {
+    for item in items {        
+        println!("{:?}", item);
+    }
+}
+
 fn main() {
-    let mut item = Item { count: 1 };
+    let mut items = vec![Item { count: 1 }];
 
-    println!("{:?}", item);
+    let first = items.get_mut(0);
+    let second = items.get_mut(1);
 
-    add_one(&mut item);
+    println!("{:?}", second);
 
-    println!("{:?}", item);
+    print_all(&items);
 }
